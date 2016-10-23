321d9823e99d13ae5c3a970a20ef22f9f472ca93
Adam J Batson <adamjbatson@gmail.com>
2016-10-23 16:46:32 -0400

    Now able to supply the bots name through command line args

--- duebot/duebot.py
+++ duebot/duebot.py
@@ -1,12 +1,14 @@
 import os
+import sys
+import time
 from slackclient import SlackClient
 
-BOT_NAME = "duebot"
 READ_SOCKET_DELAY = 1
 
 class duebot(object):
 
-	def __init__(self):
+	def __init__(self, name):
+		self.name = name
 		self.slack_client = SlackClient(os.environ.get('DUE_BOT_API_TOKEN'))
 		self.bot_id = self.get_bot_id()
 
@@ -15,14 +17,14 @@ class duebot(object):
 		if api_call.get('ok'):
 			users = api_call.get('members')
 			for user in users:
-				if 'name' in user and user.get('name') == BOT_NAME:
+				if 'name' in user and user.get('name') == self.name:
 					return user.get('id')
 			print "Could not get bot id!"
 			return -1	
 
 	def print_all_messages(self):
 		if self.slack_client.rtm_connect():
-			print "Duebot up and running!"
+			print "Duebot <" + self.name + "> up and running"
 			while True:
 				output_list = self.slack_client.rtm_read()
 				if output_list and len(output_list) > 0:
@@ -30,7 +32,15 @@ class duebot(object):
 						if 'channel' in output and 'text' in output:
 							print output['channel']
 							print output['text']
+				time.sleep(READ_SOCKET_DELAY)
+
+def Main():
+	if len(sys.argv) != 2:
+		print "Incorrect number of command line arguments!"
+		print "Usage: python duebot.py <name>"
+		return 1
+	d = duebot(sys.argv[1])
+	d.print_all_messages()
 
 if __name__ == '__main__':
-	d = duebot()
-	d.print_all_messages()
+	Main()
